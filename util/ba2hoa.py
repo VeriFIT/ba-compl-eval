400cd8364b5c19a040a223a3dce90545eb84333f
Ondra Lengal <ondra.lengal@gmail.com>
2020-08-18 10:23:21 +0200

    some initial files

---
+++ util/ba2hoa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# A script for translation of a standard Buchi automaton in Rabit's BA format
+# into the Hanoi Omega Automata format.
+
+###########################################
+if __name__ == '__main__':
+    print("ahoj")
