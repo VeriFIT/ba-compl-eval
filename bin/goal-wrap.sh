36f7ae08ab6b841a04470e98a9090efadebc1054
Ondra Lengal <ondra.lengal@gmail.com>
2020-08-24 12:49:33 +0200

    bin/: run every instance of GOAL in their own directory

--- bin/goal-wrap.sh
+++ bin/goal-wrap.sh
@@ -10,35 +10,23 @@ INPUT=$1
 shift
 params="$*"
 
+GOAL_DIR=./bin/goal
+
+GOAL_TMP_DIR="$(mktemp -d)"
+cp -r ${GOAL_DIR} ${GOAL_TMP_DIR}
+
 TMP="$(mktemp).gff"
 ./util/ba2gff.py ${INPUT} > ${TMP} || exit $?
 
+TIME_TMP="$(mktemp)"
 set -o pipefail
-out=$(./bin/goal/gc complement ${params} ${TMP} | grep -i "<state sid" | wc -l)
+# out=$(time ${GOAL_TMP_DIR}/goal/gc complement ${params} ${TMP} ${TIME_TMP} | grep -i "<state sid" | wc -l)
+out=$(/usr/bin/time -p ${GOAL_TMP_DIR}/goal/gc complement ${params} ${TMP} 2>${TIME_TMP} | grep -i "<state sid" | wc -l)
 ret=$?
 rm ${TMP}
+rm -rf ${GOAL_TMP_DIR}
 
 echo "States: ${out}"
+cat ${TIME_TMP} | grep "user" | sed "s/user/Time:/"
 
 exit ${ret}
-
-# lockdir=/tmp/goal.lock
-# while true ; do
-	# if mkdir "$lockdir" ; then    # directory did not exist, but was created successfully
-		# trap 'rm -rf "$lockdir"' 0    # remove directory when script finishes
-#
-		# TMP="$(mktemp).gff"
-		# ./util/ba2gff.py ${INPUT} > ${TMP}
-#
-		# out=$(bin/goal/gc complement ${params} ${TMP} | grep -i "<state sid" | wc -l)
-		# ret=$?
-		# rm ${TMP}
-#
-		# echo "States: ${out}"
-#
-		# exit ${ret}
-	# else
-    # echo "Waiting..."
-		# sleep 1          # wait
-	# fi
-# done
