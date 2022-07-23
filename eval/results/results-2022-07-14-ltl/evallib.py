d6d79bdf4189b7e84fb10df20045144e5c8e593e
Ondra Lengal <ondra.lengal@gmail.com>
2022-07-23 17:07:34 +0200

    improvements in evallib

--- eval/results/results-2022-07-14-ltl/evallib.py
+++ eval/results/results-2022-07-14-ltl/evallib.py
@@ -16,12 +16,12 @@ def read_file(filename):
         na_values=['ERR', 'TO', 'MISSING'])
     return df_loc
 
-
-# prints a scatter plot
-def scatter_plot(df, xcol, ycol, domain, xname=None, yname=None, log=False, width=6, height=6, clamp=True, tickCount=5):
+# A generic scatter plot command that can print several data frames into one scatterplot
+def scatter_plot_n(dfs, xcol, ycol, domain, colors, xname=None, yname=None, log=False, width=6, height=6, clamp=True, tickCount=5):
     assert len(domain) == 2
+    assert len(dfs) == len(colors)
 
-    POINT_SIZE = 1.0
+    POINT_SIZE = 1.5
     DASH_PATTERN = (0, (6, 2))
 
     if xname is None:
@@ -33,18 +33,23 @@ def scatter_plot(df, xcol, ycol, domain, xname=None, yname=None, log=False, widt
     ax_formatter = mizani.custom_format('{:n}')
 
     if clamp:  # clamp overflowing values if required
-        df = df.copy(deep=True)
-        df.loc[df[xcol] > domain[1], xcol] = domain[1]
-        df.loc[df[ycol] > domain[1], ycol] = domain[1]
+        new_df_list = list()
+        for df in dfs:
+            new_df = df.copy(deep=True)
+            new_df.loc[new_df[xcol] > domain[1], xcol] = domain[1]
+            new_df.loc[new_df[ycol] > domain[1], ycol] = domain[1]
+
+            new_df_list.append(new_df)
+
+        dfs = new_df_list
 
     # generate scatter plot
-    scatter = p9.ggplot(df)
+    scatter = p9.ggplot()
     scatter += p9.aes(x=xcol, y=ycol)
-    scatter += p9.geom_point(size=POINT_SIZE, na_rm=True)
+    for (df, color) in zip(dfs, colors):
+        scatter += p9.geom_point(size=POINT_SIZE, na_rm=True, data=df, color=color, alpha=0.5)
+        scatter += p9.geom_rug(na_rm=True, sides="tr", data=df, color=color, alpha=0.05)
     scatter += p9.labs(x=xname, y=yname)
-
-    # rug plots
-    scatter += p9.geom_rug(na_rm=True, sides="tr", alpha=0.05)
 
     if log:  # log scale
         scatter += p9.scale_x_log10(limits=domain, labels=ax_formatter)
@@ -70,6 +75,39 @@ def scatter_plot(df, xcol, ycol, domain, xname=None, yname=None, log=False, widt
     return res
 
 
+# prints a scatter plot for one data set
+def scatter_plot(df, **kwargs):
+    return scatter_plot_n(dfs=[df], colors=["black"], **kwargs)
+
+
+# scatter plot for one data set with some parameters for Buchi evaluation
+def scatplot(df, params):
+    size = 8
+    if 'xname' not in params:
+        params['xname'] = None
+    if 'yname' not in params:
+        params['yname'] = None
+    if 'max' not in params:
+        params['max'] = 10000
+    if 'tickCount' not in params:
+        params['tickCount'] = 5
+    if 'filename' not in params:
+        params['filename'] = "fig_" + params['x'] + "_vs_" + params['y']
+
+    pl = scatter_plot(df,
+                      xcol=params['x'] + '-States',
+                      ycol=params['y'] + '-States',
+                      xname=params['xname'], yname=params['yname'],
+                      domain=[1, params['max']],
+                      tickCount=params['tickCount'],
+                      log=True, width=size, height=size)
+
+    if save:
+        pl.save(filename=params['filename'], dpi=1000)
+
+    return pl
+
+
 # Print a matrix of plots
 def matrix_plot(list_of_plots, cols):
     assert len(list_of_plots) > 0
@@ -90,3 +128,98 @@ def matrix_plot(list_of_plots, cols):
                 matrix_plot &= row
 
     return matrix_plot
+
+
+# Connect a DF with results to DF with classification of inputs
+def connect_with_classification(df, clas_file):
+    df_clas = read_file(clas_file)
+    df = pd.merge(df, df_clas, on='name')
+    return df
+
+
+# prints results of classification
+def print_classification(df):
+    df_empty = df[df['empty'] == 1]
+    df_deterministic = df[df['deterministic'] == 1]
+    df_deterministic_weak = df[(df['deterministic'] == 1) & (df['weak'] == 1)]
+    df_inherently_weak = df[df['inherently weak'] == 1]
+    df_semi_deterministic = df[df['semi deterministic'] == 1]
+    df_terminal = df[df['terminal'] == 1]
+    df_unambiguous = df[df['unambiguous'] == 1]
+    df_weak = df[df['weak'] == 1]
+    df_very_weak = df[df['very weak'] == 1]
+    df_elevator = df[df['elevator'] == 1]
+    df_elevator_not_semi = df[(df['elevator'] == 1) & (df['semi deterministic'] == 0)]
+
+    print(f"! Classification of input automata")
+    print(f"!   # empty: {len(df_empty)}")
+    print(f"!   # deterministic: {len(df_deterministic)}")
+    print(f"!   # deterministic weak: {len(df_deterministic_weak)}")
+    print(f"!   # inherently weak: {len(df_inherently_weak)}")
+    print(f"!   # semi-deterministic: {len(df_semi_deterministic)}")
+    print(f"!   # terminal: {len(df_terminal)}")
+    print(f"!   # unambiguous: {len(df_unambiguous)}")
+    print(f"!   # weak: {len(df_weak)}")
+    print(f"!   # very weak: {len(df_very_weak)}")
+    print(f"!   # elevator: {len(df_elevator)}")
+    print(f"!   # elevator not semideterministic: {len(df_elevator_not_semi)}")
+
+
+# computes summary statistics
+def summary_stats(df):
+    summary = dict()
+    for col in df.columns:
+        if re.search('-States$', col) or re.search('-runtime$', col):
+            summary[col] = dict()
+            summary[col]['max'] = df[col].max()
+            summary[col]['min'] = df[col].min()
+            summary[col]['mean'] = df[col].mean()
+            summary[col]['median'] = df[col].median()
+            summary[col]['std'] = df[col].std()
+            summary[col]['timeouts'] = df[col].isna().sum()
+    return pd.DataFrame(summary).transpose()
+
+
+# table to LaTeX file
+def table_to_file(table, headers, out_file):
+    with open(f"plots/{out_file}.tex", mode='w') as fl:
+        print(tab.tabulate(table, headers=headers, tablefmt="latex"), file=fl)
+
+
+# load results
+def load_results(filename):
+    df = read_file(filename)
+
+    print(f"! Loaded results")
+    print(f"!   file:  {filename}")
+    print(f"!   time:  {datetime.datetime.now()}")
+    print(f"!   # of automata: {len(df)}")
+    return df
+
+
+# remove automata of a particular kind (e.g. inherently weak, etc.)
+def remove_auts(df, kind):
+    before = len(df)
+    df = df[df[kind] == 0]
+    after = len(df)
+    print(f"! removed {before-after} automata of the type \"{kind}\"")
+
+
+# get rid of timeouts and 0 states and give them some value
+def sanitize_results(df, df_summary_states):
+  # min and max states
+    states_min = 1
+  states_max = df_summary_states['max'].max()
+  states_timeout = states_max * 1.1
+
+  # sanitizing NAs
+  for col in df.columns:
+      if re.search('-States$', col):
+          df[col].fillna(states_timeout, inplace=True)
+          df[col].replace(0, states_min, inplace=True)  # to remove 0 (in case of log graph)
+
+      if re.search('-runtime$', col):
+          df[col].fillna(TIMEOUT_VAL, inplace=True)
+          df.loc[df[col] < TIME_MIN, col] = TIME_MIN  # to remove 0 (in case of log graph)
+
+  return df
